d36dba80b725722875f542858127af0f3aad2fde
ssnell227 <ssnell227@gmail.com>
2020-09-02 10:12:18 -0600

    more build problems

--- server/index.js
+++ server/index.js
@@ -14,11 +14,8 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')));
 
--app.get('/', function (req, res) {
-+app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
-})
+
+
 
 
 app.use(express.json())
@@ -37,6 +34,11 @@ const server = app.listen(SERVER_PORT, () => console.log(`Listening on ${SERVER_
 
 const io = require('socket.io')(server)
 require('./utils/sockets')(io)
+
+app.get('/*', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    next()
+  });
 
 //spotify endpoints
 
