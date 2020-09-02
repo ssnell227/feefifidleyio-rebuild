2d0e8fa594bcbb6686c355cc598bed3c287d15ac
ssnell227 <ssnell227@gmail.com>
2020-09-02 12:10:45 -0600

    fixes invite link

--- src/components/lobby/InviteLink/index.jsx
+++ src/components/lobby/InviteLink/index.jsx
@@ -13,7 +13,7 @@ const InviteLink = ({ gameHash }) => (
         <Toolbar>
             <Box>
                 <Typography>Invite your friends:</Typography>
-                <Typography variant='h4'> {`rebuild.feefifildey.io/landing/${gameHash}`}</Typography>
+                <Typography variant='h4'> {`https://rebuild.feefifildey.io/landing/${gameHash}`}</Typography>
             </Box>
         </Toolbar>
     </AppBar>
